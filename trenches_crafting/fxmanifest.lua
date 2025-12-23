7076025d447f36c8276c40154322467ca7e18e8c
natetrill <noddagodmuzik@gmail.com>
2025-12-23 07:42:19 -0500

    Create fxmanifest.lua

---
+++ trenches_crafting/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Yung'
+description 'Trenches RP Crafting'
+version '1.0.0'
+
+shared_script 'shared.lua'
+server_script 'server.lua'
+client_script 'client.lua'
